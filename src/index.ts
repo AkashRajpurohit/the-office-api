e8255ff6ef7dc73d1b0bae918efac76fecbd3453
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2022-11-12 11:24:24 +0530

    feat: :tada: api using honojs and cf workers

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono';
+import { prettyJSON } from 'hono/pretty-json';
+import { compress } from 'hono/compress';
+import { cors } from 'hono/cors';
+import quotes from '../data/quotes.json';
+
+const app = new Hono();
+
+// Middlewares
+app.use('*', prettyJSON());
+app.use('*', compress());
+app.use('*', cors());
+
+// Routes
+app.get('/', (c) => {
+  // For default route, redirect to github repo
+  return c.redirect(
+    'https://github.com/AkashRajpurohit/the-office-quotes-api',
+    302
+  );
+});
+
+app.get('/quote/random', (c) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return c.json(randomQuote);
+});
+
+app.notFound((c) => c.json({ message: 'Not Found' }, 404));
+
+export default app;
